0a7059ebea711100cd03a0ac627428fdc9d5d83c
Jonas Jerndin <jonas@queenslab.se>
2022-07-11 21:16:29 +0200

    Add some static

--- src/main.py
+++ src/main.py
@@ -4,8 +4,11 @@ from pydantic import BaseModel
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 quizzes = [{"id": 1, "name": "QL Quiz I test"},{"id": 2,"name": "QL Quiz II draft"}]
 
@@ -24,23 +27,7 @@ html = """
         </form>
         <ul id='messages'>
         </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
+        <script src="/static/stuff.js">
         </script>
     </body>
 </html>
@@ -67,7 +54,6 @@ class ConnectionManager:
 
 
 manager = ConnectionManager()
-
 
 
 @app.get("/quiz/list")
